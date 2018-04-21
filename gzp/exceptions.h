f66c87eb0cdf0a1fdf1366a54a6ca2579b297d55
gzpyunduan <38047636+gzpyunduan@users.noreply.github.com>
2018-04-21 16:04:11 +0800

    Add files via upload

---
+++ gzp/exceptions.h
@@ -0,0 +1,36 @@
+#ifndef _EXCEPTIONS
+#define _EXCEPTIONS
+
+#include <iostream>
+#include "String.h"
+using namespace std;
+
+class Exception {
+protected:
+	const String variant = "";
+	String detail = "";
+public:
+	Exception() {}
+	Exception(const Expection &ec) : variant(ec.variant), detail(ec.detail){}
+	virtual String what() {
+		return variant + " " + detail;
+	}
+};
+
+class index_out_of_bound : public exception {
+	/* __________________________ */
+};
+
+class runtime_error : public exception {
+	/* __________________________ */
+};
+
+class invalid_iterator : public exception {
+	/* __________________________ */
+};
+
+class container_is_empty : public exception {
+	/* __________________________ */
+};
+
+#endif
