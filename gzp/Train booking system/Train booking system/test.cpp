084a652191c9b5c18665e87ca9affde4d5f656b8
gzpyunduan <38047636+gzpyunduan@users.noreply.github.com>
2018-06-04 23:56:51 +0800

    Update test.cpp

--- gzp/Train booking system/Train booking system/test.cpp
+++ gzp/Train booking system/Train booking system/test.cpp
@@ -1,8 +1,7 @@
+#include "Bplustree.h"
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
-#include <map>
-#include "Bplustree.h"
 using namespace sjtu;
 
 long long aa = 13131, bb = 5353, MOD = (long long)(1e9 + 7), now = 1;
@@ -43,71 +42,93 @@ public:
 	}
 	Data & operator = (const Data &other) {
 		if (this == &other) return *this;
-		*x = *(other.x);
+		delete x;
+		x = new int(*(other.x));
 		return *this;
-	}
-	bool operator != (const Data &other){
-		return x == other.x;
-	}
+	} 
 	int num()const {
 		return *x;
 	}
 };
 
-
 Bplustree<Key, Data> Bpt("Bpt");
-map<Key, Data> mp;
-int num = 10;
+int num = 100;
+int dNN = 1000;
 void test_insert() {
 	puts("Test: insert");
-	std::cout << "empty: " << Bpt.empty() << std::endl;
-	std::cout << "size " << Bpt.size() << endl;
-	Key tmp[20];
-
+	std::cout << "empty:" << Bpt.empty() << std::endl;
+	std::cout << "size:" << Bpt.size() << endl;
+	int num = 100;
 	for (int i = 1; i <= num; i++) {
-		Key key(i * 10 + rand() % 10);
+		Key key(rand() % 100);
 		Data data(rand());
-		Bpt.insert(key, data);
-		mp[key] = data;
-		tmp[i] = key;
-		cout << Bpt.search(key).num() << " " << mp[key].num() << endl;
-	}
-	puts("");
-	for(int i = 1; i <= num; i++)
-		if(mp[tmp[i]] != Bpt.search(tmp[i]))
-			puts("insert error");
-}
-
-void test_erase() {
-	puts("Test: erase");
-	for (int i = 1; i <= 1000 && !Bpt.empty(); i++) {
-		int p = rand() % 1000;
-		//cout << p << endl;
-		Key key(p);
+		cout << "insert:" << key.num() << ' ' << data.num() << endl;
 		try {
-			Bpt.erase(key);
+			Bpt.insert(key, data);
 		}
-		catch (...) { /*cout << "erase error" << endl;*/ }
+		catch (...) { cout << "insert error" << endl; }
+		Bpt.Print();
 	}
 	puts("");
+	
 	for (int i = 1; i <= num; i++) {
-		int tmp = rand() % 1000;
+		int tmp = rand() % 100;
 		Key key(tmp);
 		//cout << i << ' ' << tmp << endl;
 		try {
 			std::cout << (Bpt.search(key)).num() << ' ' << endl;
 		}
+		catch (...) { cout << "index" << endl; }
+	}
+	puts("");
+	
+}
+
+void test_erase() {
+	dNN -= 1000;
+	puts("Test: erase");
+	for (int i = 1; i <= 50 && !Bpt.empty(); i++) {
+		int p = rand() % 100;
+		cout << p << endl;
+		Key key(p);
+		try {
+			Bpt.erase(key);
+		}
+		catch (...) { /*cout << "erase error" << endl;*/dNN++; }
+	}
+	puts("");
+	for (int i = 1; i <= num; i++) {
+		int tmp = rand() % 100;
+		Key key(tmp);
+		cout << i << ' ' << tmp << endl;
+		try {
+			std::cout << (Bpt.search(key)).num() << endl;
+		}
 		catch (...) { /*cout << "index" << endl;*/ }
 	}
 }
 
-void test_iterator() {
-
+void test_Iterator() {
+	puts("Test: Iterator");
+	Bplustree<Key, Data>::Iterator it(Bpt.begin());
+	int _dNN = 0;
+	while (1) {
+		try {
+			cout << it.itkey().num() << endl;
+			it++;
+		}
+		catch (...) { break; }
+	}
+	if (_dNN == dNN) puts("passed");
+	puts("");
 }
 
 int main()
 {
+	Bpt.clear();
 	test_insert();
-
+	test_erase();
+	test_Iterator();
+	system("pause");
 	return 0;
 }
